385e9e1b5f2e8db2c304dc824594bb78c2f692fa
chengzhang <chengzhang@jubao56.com>
2018-09-21 18:07:25 +0800

    update

---
+++ 2的幂/Find_test.go
@@ -0,0 +1,7 @@
+package _的幂
+
+import "testing"
+
+func TestIsPowerOfTwo(t *testing.T) {
+	println(IsPowerOfTwo(4))//true
+}
---
+++ 整数转罗马数字/To_test.go
@@ -0,0 +1,7 @@
+package 整数转罗马数字
+
+import "testing"
+
+func TestIntToRoman(t *testing.T) {
+	println(IntToRoman(90))
+}
